2a944c121beb157f529b22285b42e57d7db26453
Torsten Boettjer <torsten.boettjer@avaloq.com>
2023-08-03 23:40:04 +0200

    parameter

--- oci/config.tf
+++ oci/config.tf
@@ -64,3 +64,30 @@ locals {
     }
 }
 // --- service configuration ---//
+
+variable "account" {
+  description = "unique identifier for the oci tenancy"
+  type = string
+}
+
+variable "organisation" {
+  description = "paramater for a client"
+  type = object({
+    name   = string,
+    home   = string,
+    admin  = string
+  })
+}
+
+variable "service" {
+  description = "parameter for a service"
+  type = object({
+    name     = string,
+    contract = string,
+    owner    = string,
+    region   = string,
+    budget   = string,
+    class    = number,
+    stage    = number
+  })
+}
