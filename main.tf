b0d93e341095c828263e2a8b1e14fc500a7e6df2
Torsten Boettjer <torsten.boettjer@gmail.com>
2023-08-05 19:58:09 +0200

    compose config file

--- main.tf
+++ main.tf
@@ -1,8 +1,3 @@
 // Copyright (c) 2023 Avaloq and/or its affiliates.
 // Licensed under the Apache 2.0 license shown at https://www.apache.org/licenses/LICENSE-2.0.
 
-resource "example_thing" "iam" {
-  for_each = fileset(path.module, "iam/*.json")
-
-  # other configuration using each.value
-}
