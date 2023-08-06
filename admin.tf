90c6bd6fc20d5ff2ac50563ae29956f4d806f097
Torsten Boettjer <torsten.boettjer@gmail.com>
2023-08-06 14:06:25 +0200

    compose config file

--- admin.tf
+++ admin.tf
@@ -2,26 +2,27 @@
 // Licensed under the Apache 2.0 license shown at https://www.apache.org/licenses/LICENSE-2.0.
 
 output "oci_identity_compartment" {
-    value         = {for domain in local.domains : domain.name => {
-        name      = format("%s_%s_compartment", var.account.name, domain.name)
+    value = {for domain in local.domains : domain.name => {
+        name           = format("%s_%s_compartment", var.account.name, domain.name)
         compartment_id = var.account.parent_id
-        description    = var.compartment_description
+        description    = domain.description
         class          = var.account.class
         defined_tags   = {"budget.cost_center"= domain.cost_center}
         freeform_tags  = {"department" = domain.department}
     } if domain.stage <= var.account.stage }
 }
 
-output "groups" {
-    value     = {for group in distinct(local.users[*].role) : group => {
-        name  = format("%s_%s", var.account.name, group)
-        class = var.account.class
-        stage = var.account.stage
-    }}
+output "oci_identity_group" {
+    value = {for group in distinct(local.users[*].role) : group => {
+        name           = format("%s_%s", var.account.name, group)
+        compartment_id = var.account.parent_id
+        description    = "${group} role defined for ${var.account.parent_id}"
+        class          = var.account.class
+    } if group.stage  <= var.account.stage }
 }
 
 output "oci_identity_user" {
-    value     = {for user in local.users : user.email => {
+    value = {for user in local.users : user.email => {
         compartment_id = var.account.parent_id
         name           = user.email
         description    = format("%s %s", user.first_name, user.last_name)
