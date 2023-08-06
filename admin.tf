66fbbda9d5fc62ddc5f13331e8637b729893f3ca
Torsten Boettjer <torsten.boettjer@gmail.com>
2023-08-06 11:23:45 +0200

    compose config file

--- admin.tf
+++ admin.tf
@@ -3,7 +3,7 @@
 
 output "compartments" {
     value         = {for domain in local.domains : domain.name => {
-        name      = format("%s_%s_compartment", "${var.account.name}", "${domain.name}")
+        name      = format("%s_%s_compartment", var.account.name, domain.name)
         parent_id = var.account.parent_id
         class     = var.account.class
         stage     = var.account.stage
@@ -12,7 +12,7 @@ output "compartments" {
 
 output "groups" {
     value     = {for group in distinct(local.users[*].role) : group => {
-        name  = format("%s_%s", "${var.account.name}", "${group}")
+        name  = format("%s_%s", var.account.name, group)
         class = var.account.class
         stage = var.account.stage
     }}
@@ -26,14 +26,14 @@ output "users" {
     }}
 }
 
+/*
 output "notifications" {
     value = {for channel in local.channels : channel => {
-        name = format("%s_%s", "${var.account.name}", "${channel}")
+        name = format("%s_%s", var.account.name, channel)
         protocol  = channel.type
         endpoint  = channel.address
     } if contains(distinct(local.subscriptions[*].channels), channel.name)}
 }
-/*
 output "policies" {
      value = {for operator in local.operators : operator.name => {
         name        = "${local.service_name}_${operator.name}"
