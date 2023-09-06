270f8316741d3fa05bd4aa50cf589450992104b3
Torsten Boettjer <torsten.boettjer@avaloq.com>
2023-09-06 15:48:13 +0200

    iam json update

---
+++ net.tf
@@ -0,0 +1,22 @@
+output oci_load_balancer_load_balancer {
+    #Required
+    value = {
+        compartment_id = var.setting.parent_id
+        display_name   = format("%s_%s_compartment", var.setting.name, domain.name)
+        shape          = "Flexible"
+        subnets        = format("%s_%s_subnet_endpoint", var.setting.name, domain.name)
+
+        #Optional
+        defined_tags = {}
+        freeform_tags = {}
+        ip_mode = var.load_balancer_ip_mode
+        is_private = var.load_balancer_is_private
+        network_security_group_ids = var.load_balancer_network_security_group_ids
+        reserved_ips = {id = var.load_balancer_reserved_ips_id}
+        shape_details = {
+            #Required
+            maximum_bandwidth_in_mbps = var.load_balancer_shape_details_maximum_bandwidth_in_mbps
+            minimum_bandwidth_in_mbps = var.load_balancer_shape_details_minimum_bandwidth_in_mbps
+        }
+    }
+}
