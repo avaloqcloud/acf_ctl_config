17a7090683af2cef8026090d4da6ba82ad1da6c0
reece64 <reece.bailey@avaloq.com>
2023-09-19 16:50:24 +0200

    Include IAM entry alert

--- iam.tf
+++ iam.tf
@@ -1,6 +1,17 @@
 // Copyright (c) 2023 Avaloq and/or its affiliates.
 // Licensed under the Apache 2.0 license shown at https://www.apache.org/licenses/LICENSE-2.0.
 
+output "oci_budget_alert_rule" {
+    value = { for alert in local.alerts : alert.name => {
+        name           = format("%s_%s", var.setting.name, group)
+        compartment_id = var.setting.parent_id
+        controls       = var.setting.controls
+        description    = "${group} role defined for ${data.oci_identity_compartment.parent.name} identity domain"
+        
+    }
+    }
+}
+
 output "oci_identity_compartment" {
     value = {for domain in local.domains : domain.name => {
         name           = format("%s_%s_compartment", var.setting.name, domain.name)
@@ -23,9 +34,9 @@ output "oci_identity_group" {
 
 output "oci_identity_user" {
     value = {for user in local.users : user.email => {
+        name           = user.email
         compartment_id = var.setting.parent_id
         controls       = var.setting.controls
-        name           = user.email
         description    = format("%s %s", user.first_name, user.last_name)
         defined_tags   = {"budget.cost_center"= user.cost_center}
         email          = user.email
@@ -35,17 +46,17 @@ output "oci_identity_user" {
 
 output "oci_identity_tag_namespace" {
     value = {for namespace in local.controls : namespace.name => {
+        name           = format("%s_%s", var.setting.name, namespace.name)
         compartment_id = var.setting.parent_id
         controls       = var.setting.controls
         description    = "${namespace.name} control for ${data.oci_identity_compartment.parent.name} identity domain"
-        name           = format("%s_%s", var.setting.name, namespace.name)
     } if namespace.stage <= var.setting.stage }
 }
 
 output "oci_identity_tag" {
     value = {for tag in local.tags : tag.name => {
-        controls         = var.setting.controls
         name             = tag.name
+        controls         = var.setting.controls
         description      = "${tag.name} control"
         tag_namespace_id = local.tag_map[tag.name]
         values           = tag.values
@@ -56,9 +67,9 @@ output "oci_identity_tag" {
 
 output "oci_ons_notification_topic" {
     value = {for channel in local.channels : channel.name => {
+        name           = format("%s_%s_%s", var.setting.name, channel.service, channel.name)
         compartment_id = var.setting.parent_id
         controls       = var.setting.controls
-        name           = format("%s_%s_%s", var.setting.name, channel.service, channel.name)
         protocol       = channel.service
         endpoint       = channel.address
     } if contains(distinct(local.subscriptions[*].channel), channel.name)}
@@ -66,10 +77,10 @@ output "oci_ons_notification_topic" {
 
 output "oci_identity_policy" {
     value = {for permission in local.permissions : permission.role => {
+        name           = format("%s_%s_policy", var.setting.name, permission.role)
         compartment_id = var.setting.parent_id
         controls       = var.setting.controls
         description    = permission.policy
-        name           = format("%s_%s_policy", var.setting.name, permission.role)
         statements     = permission.permissions
     }... if contains(distinct(local.users[*].role), permission.role) }
 }
