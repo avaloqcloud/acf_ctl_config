7f8d1a9a86ccaa3bd9f9b04fa2d7cb353dc71c10
reece64 <reece.bailey@avaloq.com>
2023-09-19 16:53:37 +0200

    Include budget alert_id

--- iam.tf
+++ iam.tf
@@ -5,9 +5,9 @@ output "oci_budget_alert_rule" {
     value = { for alert in local.alerts : alert.name => {
         name           = format("%s_%s", var.setting.name, group)
         compartment_id = var.setting.parent_id
-        controls       = var.setting.controls
         description    = "${group} role defined for ${data.oci_identity_compartment.parent.name} identity domain"
-        
+        controls       = var.setting.controls
+        budget_id      = alert.budget_id
     }
     }
 }
@@ -16,8 +16,8 @@ output "oci_identity_compartment" {
     value = {for domain in local.domains : domain.name => {
         name           = format("%s_%s_compartment", var.setting.name, domain.name)
         compartment_id = var.setting.parent_id
-        controls       = var.setting.controls
         description    = domain.description
+        controls       = var.setting.controls
         defined_tags   = {"budget.cost_center"= domain.cost_center}
         freeform_tags  = {"department" = domain.department}
     } if domain.stage <= var.setting.stage }
@@ -27,8 +27,8 @@ output "oci_identity_group" {
     value = {for group in distinct(local.users[*].role) : group => {
         name           = format("%s_%s", var.setting.name, group)
         compartment_id = var.setting.parent_id
-        controls       = var.setting.controls
         description    = "${group} role defined for ${data.oci_identity_compartment.parent.name} identity domain"
+        controls       = var.setting.controls
     }}
 }
 
@@ -36,8 +36,8 @@ output "oci_identity_user" {
     value = {for user in local.users : user.email => {
         name           = user.email
         compartment_id = var.setting.parent_id
-        controls       = var.setting.controls
         description    = format("%s %s", user.first_name, user.last_name)
+        controls       = var.setting.controls
         defined_tags   = {"budget.cost_center"= user.cost_center}
         email          = user.email
         freeform_tags  = {"department" = user.department}
@@ -48,16 +48,16 @@ output "oci_identity_tag_namespace" {
     value = {for namespace in local.controls : namespace.name => {
         name           = format("%s_%s", var.setting.name, namespace.name)
         compartment_id = var.setting.parent_id
-        controls       = var.setting.controls
         description    = "${namespace.name} control for ${data.oci_identity_compartment.parent.name} identity domain"
+        controls       = var.setting.controls
     } if namespace.stage <= var.setting.stage }
 }
 
 output "oci_identity_tag" {
     value = {for tag in local.tags : tag.name => {
         name             = tag.name
-        controls         = var.setting.controls
         description      = "${tag.name} control"
+        controls         = var.setting.controls
         tag_namespace_id = local.tag_map[tag.name]
         values           = tag.values
         default          = length(flatten([tag.values])) > 1 ? element(tag.values,0) : tostring(tag.values)
@@ -79,8 +79,8 @@ output "oci_identity_policy" {
     value = {for permission in local.permissions : permission.role => {
         name           = format("%s_%s_policy", var.setting.name, permission.role)
         compartment_id = var.setting.parent_id
-        controls       = var.setting.controls
         description    = permission.policy
+        controls       = var.setting.controls
         statements     = permission.permissions
     }... if contains(distinct(local.users[*].role), permission.role) }
 }
